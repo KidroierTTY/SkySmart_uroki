ce06701635e38a84306efebde763cb00b8293633
KdroierTTY <kidroiertty@gmail.com>
2024-06-26 17:24:56 +0300

    Analizator_Texta.py Upped

--- Analizator_Texta.py
+++ Analizator_Texta.py
@@ -1,6 +1,31 @@
-from typing import Counter
+# from collections import Counter
+#
+# # подготовка текста к анализу
+#
+# text = input("Введите текст: ")
+# text = text.lower()
+#
+# punctuation = [".", ",", "!", "?"]
+#
+# for char in punctuation:
+#     text = text.replace(char, "")
+#
+# words = text.split()
+# print(words)
+# # анализ текста
+#
+# word_frequency =
+#
+# # вывод результатов
+#
+# print("Количество разных слов:", int(word_frequency.most_common()[0][1]))
+# print("Частота слов:")
+# for word, frequency in word_frequency.items():
+#     print(f"{word}: {frequency}")
 
 # подготовка текста к анализу
+
+
 
 text = input("Введите текст: ")
 text = text.lower()
@@ -14,11 +39,16 @@ words = text.split()
 
 # анализ текста
 
-word_frequency = Counter(words)
+word_frequency = {}
 
+for word in words:
+    if word in word_frequency:
+        word_frequency[word] += 1
+    else:
+        word_frequency[word] = 1
 # вывод результатов
 
-print("Количество разных слов:", int(word_frequency.most_common()[0][1]))
+print("Количество разных слов:", len(word_frequency))
 print("Частота слов:")
 for word, frequency in word_frequency.items():
     print(f"{word}: {frequency}")
